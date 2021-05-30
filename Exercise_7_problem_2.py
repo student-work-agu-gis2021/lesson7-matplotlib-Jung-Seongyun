92c19dae6ee84f6d6167107c1d9a0e54d1c13218
JungSeongyun <>
2021-05-30 06:29:27 +0000

    Loaded the Helsinki temperature data "data/helsinki-vantaa.csv"

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -17,6 +17,11 @@
 # - Parse dates from the column `'DATE'` and set the dates as index in the dataframe 
 
 # YOUR CODE HERE 1 to read the data into data and parse dates
+import pandas as pd
+import numpy as np
+import matlpotlib.pyplot as plt
+fp = 'data/helsinki-vantaa.csv'
+data = pd.read_csv(fp,prase_dates = ['DATA'],index_col = ['DATA'])
 
 # This test print should print first five rows
 print(data.head())
